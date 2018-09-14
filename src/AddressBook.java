c577503faf705d811197d56a5523c8cb10449be0
VanArman <dimaarman@gmail.com>
2018-09-14 09:22:18 -0400

    AddressBook class initialization + createBuddy(BuddyInfo obj) + removeBuddy(int index)

---
+++ src/AddressBook.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+	
+	private ArrayList<BuddyInfo> buddysList;
+	
+	public AddressBook() {
+		buddysList = new ArrayList<BuddyInfo>();
+	}
+
+	public void createBuddy() {
+		
+	}
+	
+	public void removeBuddy() {
+		
+	}
+	
+}
